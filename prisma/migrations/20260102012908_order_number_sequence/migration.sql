de3c0e335340073cc0ad5ad0bddb92a557d1af12
AhmedSaIah <ahmedsalah5446@gmail.com>
2026-01-02 03:37:10 +0200

    prisma: Update Order to generate orderNumber using sequence

---
+++ prisma/migrations/20260102012908_order_number_sequence/migration.sql
@@ -0,0 +1,12 @@
+-- Create sequence
+CREATE SEQUENCE IF NOT EXISTS order_number_seq
+START 10000
+INCREMENT 1;
+
+-- AlterTable
+ALTER TABLE "Order"
+ADD COLUMN     "orderNumber" SET DEFAULT nextval('order_number_seq');
+
+-- CreateIndex
+CREATE INDEX IF NOT EXISTS "Order_orderNumber_idx"
+    ON "Order"("orderNumber");
